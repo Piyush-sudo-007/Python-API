433583a854f3ab9213aadbf32e047d1d75da9c22
piyush dev <ootytour119@gmail.com>
2025-02-16 13:39:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -6,7 +7,10 @@ app = Flask(__name__)
 def webhook():
     data = request.get_json()
     message = data.get('message', 'No message received')
+    
     return jsonify({"Received": message}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
